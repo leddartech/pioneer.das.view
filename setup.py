836655466747e6746140d8a37a6be6a2fcb450f5
Francis Tremblay <francis.tremblay@leddartech.com>
2020-11-24 14:35:32 -0500

    feat: updates requirements in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+install_reqs = parse_requirements('requirements.txt')
+
+
 setuptools.setup(
     name="pioneer_das_view", # Replace with your own username
     version="0.4.0",
@@ -30,19 +38,6 @@ setuptools.setup(
         "https://pioneer:yK6RUkhUCNHg3e1yxGT4@svleddar-gitlab.leddartech.local/api/v4/projects/487/packages/pypi/simple/pioneer-das-api",
         "https://pioneer:yK6RUkhUCNHg3e1yxGT4@svleddar-gitlab.leddartech.local/api/v4/projects/488/packages/pypi/simple/pioneer-common-gui"
     ],
-    install_requires=[
-        'numpy',
-        'matplotlib',
-        'PyQt5==5.14',
-        'tqdm',
-        'pyyaml',
-        'pandas',
-        'opencv-python',
-        'pioneer-common>=0.4',
-        'pioneer-common-gui>=0.1',
-        'pioneer-das-api>=0.4',
-        'utm',
-        'docopt'
-    ],
+    install_requires=install_reqs,
     include_package_data = True
 )
