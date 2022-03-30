e9d79a618bbea56fb4d3aebabe29e4d24509bc5b
Simon Déziel-Nourry <simon.dn@leddatech.com>
2022-03-29 20:10:20 -0400

    [dasview.py] Add das.api logger cmdline argument

--- pioneer/das/view/apps/dasview.py
+++ pioneer/das/view/apps/dasview.py
@@ -1,23 +1,26 @@
-"""dasview v0.1
+"""dasview
+
 Usage:
   dasview <folder>
-  dasview <folder> [options] 
+  dasview <folder> [options]
+  dasview (-h | --help | --version)
+  Use --help for more info
 
 Options:
--h --help                show this help message and exit
+-h, --help               show this help message and exit
   --version              show version and exit
   --include=<inc>        exclusively included sensors in platform [default: ]
   --ignore=<ign>         ignored sensors in platform [default: ]
   --add_sync=<source>    given source will also be synchronized [default: ]
   --video_recording_enable        desactivate the qt multi-threading, and then enable frame grabing for video recording
   --video_fps=<int>      force video fps to a specific value, else compute from datasource timestamps
+-l, --log                activate das.api logger
 """
 
 from pioneer.das.api import platform
 from pioneer.das.view.viewer import Viewer
 
-from docopt import docopt
-
+import docopt
 import os
 
 DEFAULT_IGNORE_LABELS = ['radarTI_bfc']
@@ -25,7 +28,12 @@ DEFAULT_IGNORE_LABELS = ['radarTI_bfc']
 def main():
     import os
     version = '0.1'
-    args = docopt(__doc__, version = version)
+
+    try:
+      args = docopt.docopt(__doc__, version = f'dasview {version}')
+    except docopt.DocoptExit as e:
+      print(e)
+      exit()
 
     path = args['<folder>']
 
@@ -36,6 +44,7 @@ def main():
     add_sync = args['--add_sync'].split(',')
     add_sync = None if add_sync==[''] else int(add_sync)
     video_fps = args['--video_fps']
+    use_logger = args['--log'] or args['-l']
 
     if args['--video_recording_enable']:
       
@@ -53,7 +62,7 @@ def main():
       """)
 
     os.environ['QSG_RENDER_LOOP'] = "basic" #to set breakpoint in render thread...
-    pf = platform.Platform(path, default_cache_size=1)
+    pf = platform.Platform(path, default_cache_size=1, activate_logger=use_logger)
     v = Viewer(None, platform = pf, include=include, ignore=ignore, add_sync=add_sync, video_fps=video_fps)
     v.run()
 
